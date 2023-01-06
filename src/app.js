ee84fd355cd537b42fe9f8b9a996230f6e6e2bf4
erico <ecocaval@hotmail.com>
2023-01-06 00:30:01 -0300

    added tweets post and get methods

--- src/app.js
+++ src/app.js
@@ -13,22 +13,39 @@ app.listen(PORT, () => console.log(`Running on port ${PORT}`))
 app.post("/sign-up", (req, res) => {
     const user = req.body
     serverUsers.push(user)
-    console.log(serverUsers);
     res.send("OK")
 })
 
 app.post("/tweets", (req, res) => {
     const fullTweet = req.body
-    const userRegistered = serverUsers.find(item => item.userName === fullTweet.userName)
+    const userRegistered = serverUsers.find(item => item.username === fullTweet.username)
     
     if(userRegistered) {
-        serverTweets.push(fullTweet.tweet)
-        console.log(serverTweets);
+        serverTweets.push(fullTweet)
         return res.send("OK") 
     }
+
     res.send("UNAUTHORIZED")
 })
 
-app.get("/tweets", (req, res) => {
+app.get("/tweets", (_, res) => {
+    let lastTweets = []
+    const recentTweets = serverTweets.reverse()
 
+    if(recentTweets) {
+        if(recentTweets.length > 10) {
+            recentTweets = recentTweets.slice(0, 10)
+        }
+    
+        lastTweets = recentTweets.map(item => {
+            const nameAndAvatar = serverUsers.find(_item => _item.username === item.username)
+    
+            return {
+                username: item.username,
+                avatar: nameAndAvatar.avatar,
+                tweet: item.tweet
+            }
+        })
+        res.send(lastTweets)
+    }  
 })
