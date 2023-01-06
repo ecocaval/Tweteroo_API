8beeb11004230404d8ac3a2c6ebc16f3cce34922
erico <ecocaval@hotmail.com>
2023-01-05 23:56:12 -0300

    adding tweets post function

--- src/app.js
+++ src/app.js
@@ -1,15 +1,34 @@
 import express from "express"
 import cors from "cors"
 
-const PORT = 5000
 const serverUsers = []
+const serverTweets = []
+
+const PORT = 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.listen(PORT, () => console.log(`Running on port ${PORT}`))
 
 app.post("/sign-up", (req, res) => {
-    const user = req.body 
+    const user = req.body
     serverUsers.push(user)
+    console.log(serverUsers);
     res.send("OK")
+})
+
+app.post("/tweets", (req, res) => {
+    const fullTweet = req.body
+    const userRegistered = serverUsers.find(item => item.userName === fullTweet.userName)
+    
+    if(userRegistered) {
+        serverTweets.push(fullTweet.tweet)
+        console.log(serverTweets);
+        return res.send("OK") 
+    }
+    res.send("UNAUTHORIZED")
+})
+
+app.get("/tweets", (req, res) => {
+
 })
