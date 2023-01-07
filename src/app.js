4e961051f23cf7d4a86c0ccdf39ce6a678ada75c
erico <ecocaval@hotmail.com>
2023-01-06 22:54:40 -0300

    adding requirement GET /tweets with pagination

--- src/app.js
+++ src/app.js
@@ -7,10 +7,13 @@ const app = express()
 const serverUsers = []
 const serverTweets = []
 
-let lastTweets = []
 let allTweets = []
+let pageTweets = []
 
 function addAvatarToTweets(refTweets) {
+
+    if (!refTweets) return
+
     const refTweetsWithAvatar = refTweets.map(item => {
         const nameAndAvatar = serverUsers.find(_item => _item.username === item.username)
         return {
@@ -22,20 +25,36 @@ function addAvatarToTweets(refTweets) {
     return refTweetsWithAvatar
 }
 
+function divideTweets(tweetsToDivide, arrSize) {
+    const dividedTweets = []
+
+    let mainCounter = 1 // starts with 1 to match urlPage when loading tweets
+    let auxCounter = 0
+
+    for (let i = 0; i < tweetsToDivide.length; i += arrSize) {
+        dividedTweets[mainCounter] = []
+        do {
+            dividedTweets[mainCounter].push(tweetsToDivide[auxCounter])
+            auxCounter++
+        } while (dividedTweets[mainCounter].length < arrSize && tweetsToDivide[auxCounter])
+        mainCounter++
+    }
+    return dividedTweets
+}
+
 app.use(cors())
 app.use(express.json())
 app.listen(PORT, () => console.log(`Running on port ${PORT}`))
 
 app.post("/sign-up", (req, res) => {
     const user = req.body
-    const { username, avatar } = req.body
 
-    if (!username || !avatar) {
+    if (!user.username || !user.avatar) {
         return res.status(400).send("Todos os campos são obrigatórios!")
     }
 
-    const inputsAreEmpty = !username || !avatar
-    const inputsAreStrings = (typeof username === "string" && typeof avatar === "string")
+    const inputsAreEmpty = !user.username || !user.avatar
+    const inputsAreStrings = (typeof user.username === "string" && typeof user.avatar === "string")
 
     if (inputsAreEmpty || !inputsAreStrings) {
         return res.sendStatus(400)
@@ -46,19 +65,13 @@ app.post("/sign-up", (req, res) => {
 })
 
 app.post("/tweets", (req, res) => {
-    const { tweet } = req.body
     const fullTweet = req.body
+    const tweetIsEmpty = !fullTweet.tweet
+    const tweetIsString = (typeof fullTweet.tweet === "string")
 
-    const tweetIsEmpty = !tweet
-    const tweetIsString = (typeof tweet === "string")
+    if (tweetIsEmpty) return res.status(400).send("Todos os campos são obrigatórios!")
 
-    if (tweetIsEmpty) {
-        return res.status(400).send("Todos os campos são obrigatórios!")
-    }
-
-    if (!tweetIsString) {
-        return res.sendStatus(400)
-    }
+    if (!tweetIsString) return res.sendStatus(400)
 
     const userRegistered = serverUsers.find(item => item.username === fullTweet.username)
 
@@ -67,17 +80,38 @@ app.post("/tweets", (req, res) => {
         return res.status(201).send("OK")
     }
 
-    res.status(401).send("UNAUTHORIZED")
+    return res.status(401).send("UNAUTHORIZED")
 })
 
-app.get("/tweets", (_, res) => {
-    allTweets = serverTweets.reverse()
+app.get("/tweets", (req, res) => {
+
+    let dividedTweets = []
+
+    const { query } = req
+    const urlPage = Number(query.page)
+
+    if (urlPage <= 0) return res.status(400).send("Informe uma página válida!")
+
+    allTweets = [...serverTweets].reverse()
 
     if (allTweets) {
-        if (allTweets.length > 10) allTweets = allTweets.slice(0, 10)
 
-        lastTweets = addAvatarToTweets(allTweets)
-        res.send(lastTweets)
+        if (!urlPage) {
+            pageTweets = addAvatarToTweets(allTweets.slice(0, 10))
+            return res.send(pageTweets)
+        }
+
+        if (allTweets.length > 10) {
+            dividedTweets = divideTweets(allTweets, 10)
+
+            if (dividedTweets) pageTweets = addAvatarToTweets(dividedTweets[urlPage])
+
+            if (pageTweets) return res.send(pageTweets)
+            else return res.send([])
+        } else {
+            pageTweets = addAvatarToTweets(allTweets)
+            return res.send(pageTweets)
+        }
     }
 })
 
