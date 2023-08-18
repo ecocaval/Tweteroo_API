64787b924a2e6b7d8879dfe2682b4a5ed43addff
erico cavalcanti <ecocaval@hotmail.com>
2023-08-18 18:50:05 -0300

    alterando pra aulinha

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import chalk from "chalk"
 
 const PORT = 5000
 const app = express()
@@ -10,41 +11,13 @@ const serverTweets = []
 let allTweets = []
 let pageTweets = []
 
-function addAvatarToTweets(refTweets) {
-
-    if (!refTweets) return
-
-    const refTweetsWithAvatar = refTweets.map(item => {
-        const nameAndAvatar = serverUsers.find(_item => _item.username === item.username)
-        return {
-            username: item.username,
-            avatar: nameAndAvatar.avatar,
-            tweet: item.tweet
-        }
-    })
-    return refTweetsWithAvatar
-}
-
-function divideTweets(tweetsToDivide, arrSize) {
-    const dividedTweets = []
-
-    let mainCounter = 1 // starts with 1 to match urlPage when loading tweets
-    let auxCounter = 0
-
-    for (let i = 0; i < tweetsToDivide.length; i += arrSize) {
-        dividedTweets[mainCounter] = []
-        do {
-            dividedTweets[mainCounter].push(tweetsToDivide[auxCounter])
-            auxCounter++
-        } while (dividedTweets[mainCounter].length < arrSize && tweetsToDivide[auxCounter])
-        mainCounter++
-    }
-    return dividedTweets
-}
-
 app.use(cors())
 app.use(express.json())
-app.listen(PORT, () => console.log(`Running on port ${PORT}`))
+
+app.listen(PORT, () => {
+    console.log(chalk.yellow(`Running on port ${PORT}\n`))
+    console.log(chalk.blue(`Aulinha para `) + chalk.blue(`Faetec!!! Boa noite!`))
+})
 
 app.post("/sign-up", (req, res) => {
     const user = req.body
@@ -132,3 +105,40 @@ app.get("/tweets/:username", (req, res) => {
 
     res.send(userTweets)
 })
+
+app.get("/faetec-2023", (req, res) => {
+    res.send({text: "Obrigado por assistirem a aula!"})
+})
+
+
+function addAvatarToTweets(refTweets) {
+
+    if (!refTweets) return
+
+    const refTweetsWithAvatar = refTweets.map(item => {
+        const nameAndAvatar = serverUsers.find(_item => _item.username === item.username)
+        return {
+            username: item.username,
+            avatar: nameAndAvatar.avatar,
+            tweet: item.tweet
+        }
+    })
+    return refTweetsWithAvatar
+}
+
+function divideTweets(tweetsToDivide, arrSize) {
+    const dividedTweets = []
+
+    let mainCounter = 1 // starts with 1 to match urlPage when loading tweets
+    let auxCounter = 0
+
+    for (let i = 0; i < tweetsToDivide.length; i += arrSize) {
+        dividedTweets[mainCounter] = []
+        do {
+            dividedTweets[mainCounter].push(tweetsToDivide[auxCounter])
+            auxCounter++
+        } while (dividedTweets[mainCounter].length < arrSize && tweetsToDivide[auxCounter])
+        mainCounter++
+    }
+    return dividedTweets
+}
